c9c2316e44abaa4161c293ea01d99b77d58f7143
Alius Petraska <alius.petraska@ibm.com>
2017-11-10 15:04:26 +0200

    update;

--- LapsWebApi/Startup.cs
+++ LapsWebApi/Startup.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace LapsWebApi
 {
@@ -34,6 +28,8 @@ namespace LapsWebApi
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
